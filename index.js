1a5bad0e8d6bb8c15a173333eb1a0e927cf727eb
SCENECS <scenecs@t-online.de>
2015-12-01 10:09:35 +0100

    Implements option handling
    -

--- index.js
+++ index.js
@@ -6,85 +6,122 @@
 
 'use strict';
 
-var through2 = require("through2"),
+var extend = require("extend"),
+    through2 = require("through2"),
     canCompiler = require("can-compile"),
     fs = require("fs"),
     path = require("path"),
 
 
+    /**
+     * can-compile needs jQuery and canJs to compile the templates. By default, it loads all files down.
+     * Thus always an Internet connection necessary.
+     *
+     *
+     * The attribute "isVendorScriptsInstalled" is true, if the needed vendors are installed.
+     *
+     * This browserify plugin expects the following file structure:
+     *    - vendor/jquery
+     *    - vendor/canjs
+     *
+     * @attribute isVendorScriptsInstalled
+     * @type boolean
+     * @default false
+     */
+    isVendorScriptsInstalled = false,
+    
+    isNone = function isNone(obj) {
+      return obj === null || obj === undefined;
+    },
+
     createFileExtensionRegExp = function createFileExtensionRegExp(fileExtensions){
       var _fileExtensions = [],
           _flags = "ig";
       
-      switch(true) {
-        case ("string" == typeof fileExtensions):
-          _fileExtensions = (fileExtensions.split(new RegExp("[,;]"))).map(function(item, index){
-            return item.replace(new RegExp("[^\\w-]", _flags), "");
-          });
-          break;
-
-        case (Array.isArray(fileExtensions)):
-          _fileExtensions = fileExtensions.map(function(item, index){
-            return item.replace(new RegExp("[^\\w-]", _flags), "");
-          });
-          break;
-
-        default:
-          _fileExtensions.push("*");
+      if(Array.isArray(fileExtensions)) {
+        _fileExtensions = fileExtensions;
+      }
+      
+      if("string" == typeof fileExtensions) {
+        _fileExtensions = fileExtensions.split(new RegExp("[,;]"));
       }
 
-      return new RegExp("^.*\\/([\\w-]+)\\.(" + _fileExtensions.join("|") + ")$", _flags);
+      _fileExtensions = _fileExtensions.map(function(item, index){
+        return item.replace(new RegExp("[^\\w-]", _flags), "");
+      });
+
+      return new RegExp("^.*\\" + path.sep + "([\\w-]+)\\.(" + (_fileExtensions.join("|") || "[\\w]+") + ")$", _flags);
+    },
+    
+    
+    canCompileDefaultNormalizer = function(fileExtensions){
+      var _fileExtensions = fileExtensions;
+
+      return function normalizer(file){
+        var fileExtRegEx = createFileExtensionRegExp(_fileExtensions),
+            filename;
+
+        try {
+          filename = (fileExtRegEx.exec(file))[1];
+        } catch(err) {
+          filename = file;
+        }
+
+        return filename;
+      }
     };
 
+
+// checks, if vendors are installed
+try {
+  if(!isNone(fs.statSync(path.resolve(__dirname + "/vendor")))) {
+    isVendorScriptsInstalled = true;
+  }
+} catch(error) {
+  isVendorScriptsInstalled = false;
+}
+
+
 module.exports = function canCompile(bundle, options){
-
-  let _options = {
-    "fileExtensions": ["stache", "ejs", "mustache"],
-    "basedir": process.cwd(),
-    "dest": null,
-  };
-
-  Object.keys(options).forEach(function(key) {
-
-    switch(key) {
-      case "basedir":
-        _options.basedir = options[key];
-        break;
-      case "fileExtensions":
-        _options.fileExtensions = options[key];
-        break;
-    }
-  });
   
+  var _options = {
+        "extensions": {
+          "stache": "stache",
+          "ejs": "ejs",
+          "mustache": "mustache"
+        },
+        "paths": undefined,
+        "filename": undefined,
+        "version": "2.3.2",
+        "normalizer": canCompileDefaultNormalizer()
+      };
+
+  if(true === isVendorScriptsInstalled) {
+    _options.paths = {
+      'jquery': path.resolve(__dirname + "/vendor/jquery/dist/jquery.js"),
+      'can': path.resolve(__dirname + "/vendor/canjs/can.jquery.js"),
+      'ejs': path.resolve(__dirname + "/vendor/canjs/can.ejs.js"),
+      'mustache': path.resolve(__dirname + "/vendor/canjs/can.view.mustache.js"),
+      'stache': path.resolve(__dirname + "/vendor/canjs/can.stache.js")
+    };
+  }
+
   bundle.pipeline.get("deps").splice(1, 0, through2.obj(function(row, enc, next) {
-      var file = row["id"],
-          fileExtRegEx = createFileExtensionRegExp(_options.fileExtensions);
-  
+      _options = extend(true, {}, _options, options);
+    
+      var _this = this,
+          file = row["id"],
+          fileExtRegEx = createFileExtensionRegExp(Object.keys(_options.extensions));
 
+      this.bpccOptions = _options;
+        
       if(fileExtRegEx.test(file)) {
 
-        if("undefined" == typeof this.chunkBuffer) {
-          this.chunkBuffer = [];
+        if("undefined" == typeof this.bpccBuffer) {
+          this.bpccBuffer = [];
         }
         
-        var _this = this,
-            _canCompileOptions = {
-              "version": '2.3.2',
-              "normalizer": function(file){
-                var fileExtRegEx = createFileExtensionRegExp(_options.fileExtensions),
-                    filename;
-
-                try {
-                  filename = (fileExtRegEx.exec(file))[1];
-                } catch(err) {
-                  filename = file;
-                }
-
-                return filename;
-              }
-            };
-        
-        canCompiler(file, _canCompileOptions, function (err, result){
+        canCompiler(file, _options, function (err, result){
 
           if(err) {
             return next(err);
@@ -92,11 +129,11 @@ module.exports = function canCompile(bundle, options){
  
           var bufferedResult = new Buffer(result);
 
-          if(null === _options.dest){
+          if(isNone(_options.filename)){
             row["source"] = bufferedResult;
             _this.push(row);
           } else {
-            _this.chunkBuffer.push(bufferedResult);
+            _this.bpccBuffer.push(bufferedResult);
           }
           
           next();
@@ -109,13 +146,15 @@ module.exports = function canCompile(bundle, options){
     },
     function flush(done) {
       
-      if(null !== _options.dest) {
-        var chunkBuffer = this.chunkBuffer;
-        
-        if(Array.isArray(chunkBuffer)) {
+      var _options = this.bpccOptions;
+
+      if(!isNone(_options.filename)) {
+        var bpccBuffer = this.bpccBuffer;
+
+        if(Array.isArray(bpccBuffer)) {
           var writeStream = fs.createWriteStream(path.normalize(_options.dest), { "flags": "w", "defaultEncoding": "utf8" });
           
-          chunkBuffer.forEach(function(item, index){
+          bpccBuffer.forEach(function(item, index){
             writeStream.write(item);
           });
           
